aba5edc448fba2cdf7acf113ce36ea460bfe3eac
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-05-04 17:48:07 +0200

    [Fix last commit]

--- src/data/store.js
+++ src/data/store.js
@@ -1,7 +1,10 @@
 import { reactive } from "vue"
 
 export const store = reactive({
-
-
-    
+    originalApiUrl:                 "https://api.themoviedb.org/3/search/movie",
+    apiUrl :                        "https://api.themoviedb.org/3/search/movie",
+    api_key :                       "8a9378730c1534547b3d8231e788ded6",
+    language:                       "it-IT",
+    titleToSearch:                  "Il signore degli anelli",
+    resultApiCall :                  [],
 })
