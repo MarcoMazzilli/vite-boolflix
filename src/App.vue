f13c237df5afea79dadc58cff29d433f9cdcb52d
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-05-04 17:47:33 +0200

    [Add] function getApi

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import Header            from './components/Header.vue'
 import Main              from './components/Main.vue'
 import Footer            from './components/Footer.vue'
 import SearchSection     from './components/MicroComponent/SearchSection.vue'
+import { store }         from './data/store'
+import axios from 'axios'
 
 
 
@@ -12,11 +14,31 @@ import SearchSection     from './components/MicroComponent/SearchSection.vue'
       SearchSection,
       Main,
       Footer,
+      store,
     },
     data(){
       return{
         name : "appVue"
       }
+    },
+    methods:{
+      getApi(){
+        axios.get(store.apiUrl,{
+          params:{
+            api_key : store.api_key,
+            language : store.language,
+            query : store.titleToSearch
+          }
+        })
+        .then(result => {
+          store.resultApiCall = result.data
+          console.log('result.data.results',result.data.results);
+          console.log('store.resultApiCall -->',store.resultApiCall);
+        })
+      }
+    },
+    mounted(){
+      this.getApi()
     }
   }
   </script>
